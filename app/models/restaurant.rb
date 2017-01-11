ec64af432c01c8513040fd1c690c95f6d443f83f
Pea M. Tyczynska <CrystalPea@github.com>
2017-01-11 17:24:41 +0000

    All tests pass

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,5 @@
 class Restaurant < ApplicationRecord
   belongs_to :user
   has_many :reviews, dependent: :destroy
-  has_many :reviewed_restaurants, through: :reviews, source: :restaurant
   validates :name, length: {minimum: 3}, uniqueness: true
 end
