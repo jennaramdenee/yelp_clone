80dd02ace202056e90d25aa1ca1a14a10476f913
Pea M. Tyczynska <CrystalPea@github.com>
2017-01-11 13:50:25 +0000

    App upp and running with facebook on development mode

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,12 @@ class User < ApplicationRecord
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      require 'pry'; binding.pry
+      
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
       user.name = auth.info.name
-      user.image = auth.info.image
+      # user.image = auth.info.image
     end
       # If you are using confirmable and the provider(s) you use validate emails,
       # Uncomment the line below to skip the confirmation emails.
