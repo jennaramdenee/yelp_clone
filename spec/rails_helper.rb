afcf5138973b6c7e12eb1c6513e39911a35b8736
Jenna Ramdenee <jenna_ramdenee@hotmail.co.uk>
2017-01-13 12:09:35 +0000

    Refactored code to speed up slow rspec tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,7 +5,8 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
-require 'capybara/rails'
+require 'capybara'
+require_relative 'features/web_helpers'
 require 'capybara/poltergeist'
 Capybara.javascript_driver = :poltergeist
 require 'support/database_cleaner'
